f68e5a55fceab99fdc79f863dd6da0fa707652e8
michaelyoussef396 <mihcaelyoussef396@gmail.com>
2024-12-09 15:21:13 +1100

    fixed styling in wobble card

--- components/ui/wobble-card.tsx
+++ components/ui/wobble-card.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
-import { debounce } from "lodash";
 
 export const WobbleCard = ({
   children,
@@ -16,19 +15,13 @@ export const WobbleCard = ({
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [isHovering, setIsHovering] = useState(false);
 
-  // Debounced function to handle mouse movement
   const handleMouseMove = (event: React.MouseEvent<HTMLElement>) => {
-    const target = event.currentTarget;
-    if (!target) return;
-  
-    const rect = target.getBoundingClientRect();
-    const x = (event.clientX - (rect.left + rect.width / 2)) / 20;
-    const y = (event.clientY - (rect.top + rect.height / 2)) / 20;
-  
+    const { clientX, clientY } = event;
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = (clientX - (rect.left + rect.width / 2)) / 20;
+    const y = (clientY - (rect.top + rect.height / 2)) / 20;
     setMousePosition({ x, y });
   };
-  
-
   return (
     <motion.section
       onMouseMove={handleMouseMove}
@@ -44,12 +37,12 @@ export const WobbleCard = ({
         transition: "transform 0.1s ease-out",
       }}
       className={cn(
-        "mx-auto w-full bg-gold relative rounded-2xl overflow-hidden",
+        "mx-auto w-full bg-gold  relative rounded-2xl overflow-hidden",
         containerClassName
       )}
     >
       <div
-        className="relative h-full [background-image:radial-gradient(88%_100%_at_top,rgba(255,255,255,0.5),rgba(255,255,255,0))] sm:mx-0 sm:rounded-2xl overflow-hidden"
+        className="relative  h-full [background-image:radial-gradient(88%_100%_at_top,rgba(255,255,255,0.5),rgba(255,255,255,0))]  sm:mx-0 sm:rounded-2xl overflow-hidden"
         style={{
           boxShadow:
             "0 10px 32px rgba(34, 42, 53, 0.12), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.05), 0 4px 6px rgba(34, 42, 53, 0.08), 0 24px 108px rgba(47, 48, 55, 0.10)",
